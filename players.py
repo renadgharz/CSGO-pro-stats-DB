b7b72211c6fbfd85cde966621fd893486e72da34
Renad <renad.gharz@outlook.com>
2022-05-27 11:17:21 -0600

    faulty file

--- players.py
+++ players.py
@@ -1,23 +1,22 @@
+import time
+
 import pandas as pd,bs4, requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-
-url = 'https://www.hltv.org/stats/players'
-players_url = requests.get(url)
-soup = bs4.BeautifulSoup(players_url.text, 'html.parser')
 
 driver = webdriver.Edge()
-driver.get(url)
 driver.implicitly_wait(120)
-driver.find_element(By.ID,'CybotCookiebotDialogBodyButtonDecline').click()
-
 
 def get_players_overview():
 
-    players_overview = pd.read_html(players_url.text)[0]
+    players_url = 'https://www.hltv.org/stats/players'
+
+    driver.get(players_url)
+    time.sleep(10)
+    driver.find_element(By.ID, 'CybotCookiebotDialogBodyButtonDecline').click()
+
+    players_overview = pd.read_html(requests.get(players_url).text)[0]
 
     driver.find_element(By.XPATH,
                         value='/html/body/div[2]/div[1]/div[2]/div[1]/div[2]/div[4]/div/div/a[2]').click()
@@ -27,12 +26,18 @@ def get_players_overview():
                         value='/html/body/div[2]/div[1]/div[2]/div[1]/div[2]/div[4]/div/div/a[3]').click()
     players_overview_t = pd.read_html(requests.get(driver.current_url).text)[0]
 
+    driver.quit()
+
 
 def get_players_flashes():
 
-    driver.find_element(by=By.XPATH,
-                        value='/html/body/div[2]/div[1]/div[2]/div[2]/div[2]/div[2]/div[2]/div/a[4]').click()
-    players_flashes = pd.read_html(requests.get(driver.current_url).text)[0]
+    players_flashes_url = 'https://www.hltv.org/stats/players/flashbangs'
+
+    driver.get(players_flashes_url)
+    time.sleep(10)
+    driver.find_element(By.ID, 'CybotCookiebotDialogBodyButtonDecline').click()
+
+    players_flashes = pd.read_html(requests.get(players_flashes_url).text)[0]
 
     driver.find_element(By.XPATH,
                         value='/html/body/div[2]/div[1]/div[2]/div[1]/div[2]/div[4]/div/div/a[2]').click()
@@ -42,8 +47,18 @@ def get_players_flashes():
                         value='/html/body/div[2]/div[1]/div[2]/div[1]/div[2]/div[4]/div/div/a[3]').click()
     players_flashes_t = pd.read_html(requests.get(driver.current_url).text)[0]
 
+    driver.quit()
+
 
 def get_players_op_kills():
+
+    players_op_kills_url = 'https://www.hltv.org/stats/players/openingkills'
+
+    driver.get(players_op_kills_url)
+    time.sleep(10)
+    driver.find_element(By.ID, 'CybotCookiebotDialogBodyButtonDecline').click()
+
+    players_op_kills = pd.read_html(requests.get(players_op_kills_url).text)[0]
 
     driver.find_element(by=By.XPATH,
                         value='/html/body/div[2]/div[1]/div[2]/div[2]/div[2]/div[2]/div[2]/div/a[5]').click()
@@ -57,8 +72,18 @@ def get_players_op_kills():
                         value='/html/body/div[2]/div[1]/div[2]/div[1]/div[2]/div[4]/div/div/a[3]').click()
     players_op_kills_t = pd.read_html(requests.get(driver.current_url).text)[0]
 
+    driver.quit()
+
 
 def get_players_pistols():
+
+    players_pistols_url = 'https://www.hltv.org/stats/players/pistols'
+
+    driver.get(players_pistols_url)
+    time.sleep(10)
+    driver.find_element(By.ID, 'CybotCookiebotDialogBodyButtonDecline').click()
+
+    players_pistols = pd.read_html(requests.get(players_pistols_url).text)[0]
 
     driver.find_element(by=By.XPATH,
                         value='/html/body/div[2]/div[1]/div[2]/div[2]/div[2]/div[2]/div[2]/div/a[6]').click()
@@ -72,5 +97,10 @@ def get_players_pistols():
                         value='/html/body/div[2]/div[1]/div[2]/div[1]/div[2]/div[4]/div/div/a[3]').click()
     players_pistols_t = pd.read_html(requests.get(driver.current_url).text)[0]
 
+    driver.quit()
 
-driver.quit()
+
+get_players_overview()
+get_players_flashes()
+get_players_op_kills()
+get_players_pistols()
